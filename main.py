7faa43ada0a4b35b0ec7f118ed82a338bf6fa7f6
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-07-14 21:06:48 +0600

    Enhanced

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ pathOut = '/edit'
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
     edit = img.filter(ImageFilter.SHARPEN)
+
+    fact = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(fact)
     clean_name = os.path.splitext(filename)[0]
     edit.save(f'.{pathOut}/{clean_name}_edited.png')
