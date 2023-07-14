e0b29433e9ef560374776de188b22a395c0fd596
Zyan <fislam183070@bscse.uiu.ac.bd>
2023-07-14 21:04:32 +0600

    Update

---
+++ main.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = '/edit'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    edit = img.filter(ImageFilter.SHARPEN)
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f'.{pathOut}/{clean_name}_edited.png')
